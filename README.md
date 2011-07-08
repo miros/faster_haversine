f9217ecf71235855b4e621a3add84eef9deac552
miros <mirosm@mirosm.ru>
2011-07-08 23:07:29 +0400

    specs and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+==== Calculation of great-circle distance by Haversine formula implemented as C extension
+
+Usage:
+
+    FasterHaversine.distance(latitude1, longitude1, latitude2, longitude2)
--- spec/faster_haversine_spec.rb
+++ spec/faster_haversine_spec.rb
@@ -3,15 +3,26 @@ require 'spec_helper'
 describe FasterHaversine do
 
   describe ".distance" do
-    it "works" do
-      ap described_class.distance(20, 50, 20.12345, 50.12345)
+    it "calculates distances" do
+      first_point = [25.12345, 50.12345]
+
+      expected_distances_to = {
+        first_point => 0,
+        [25, 50] => 18,
+        [10, 10] => 4552,
+        [85.321, 75.321] => 6747
+      }
+
+      expected_distances_to.each do |coords, distance|
+        described_class.distance(*(first_point + coords)).should be_within(1).of(distance)
+      end
     end
   end
 
   it "works as module method" do
     object = Object.new
     object.extend(described_class)
-    ap object.send(:distance, 20, 50, 20.12345, 50.12345)
+    object.send(:distance, 12, 34, 56, 78).should be_instance_of(Float)
   end
 
 end
